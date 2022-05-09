c475009716ab409bdeee7cd417a9a29cfa6199e6
uk-m <mymk426@kyf.biglobe.ne.jp>
2022-05-09 14:08:06 +0000

    ログイン機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_30_120003) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_09_114024) do
   create_table "answers", force: :cascade do |t|
     t.string "name"
     t.text "content"
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_30_120003) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
