469279816380e2c8c24e72ca668f006c5872039a
uk-m <mymk426@kyf.biglobe.ne.jp>
2022-05-13 13:35:19 +0000

    remember me追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to root_path
     else
-      flash.now[:danger] = '正しい情報を入力してください'
       render 'new'
     end
   end
 
   def destroy
-    session.delete(:user_id)
+    log_out if logged_in?
     redirect_to root_path, status: :see_other
   end
 end
