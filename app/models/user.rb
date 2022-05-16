7338accb90ec0e8d76be160f68b16ba628c4f111
uk-m <mymk426@kyf.biglobe.ne.jp>
2022-05-16 14:24:30 +0000

    users edit追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
     length: { maximum: 40 },
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
   
   def remember
     self.remember_token = User.new_token
