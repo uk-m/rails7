91c0599e2f7ed4d56a138be9521f056b5024d8ed
uk-m <mymk426@kyf.biglobe.ne.jp>
2022-05-20 10:48:25 +0000

    質問回答モデル修正

--- db/migrate/20220222040801_create_questions.rb
+++ db/migrate/20220222040801_create_questions.rb
@@ -1,7 +1,6 @@
 class CreateQuestions < ActiveRecord::Migration[7.0]
   def change
     create_table :questions do |t|
-      t.string :name, null: false
       t.string :title, null: false
       t.text :content, null: false
 
