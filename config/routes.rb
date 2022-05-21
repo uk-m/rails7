6366de25b963221dda3fe5bf95a9a461636082b8
uk-m <mymk426@kyf.biglobe.ne.jp>
2022-05-21 13:38:30 +0000

    user削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   post 'logout', to: 'sessions#destroy'
   
   root "questions#index"
-  resources :users, only: %i(new create show edit update)
+  resources :users, only: %i(new create show edit update destroy)
   resources :questions do
     resources :answers
   end
