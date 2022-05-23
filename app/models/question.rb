1b7b894d143b7dc4d80975075a50fefc8ebd9a5a
uk-m <mymk426@kyf.biglobe.ne.jp>
2022-05-23 12:43:24 +0000

    質問機能をuserに関連付け

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ApplicationRecord
+  belongs_to :user
+  validates :user_id, presence: true
+  default_scope -> { order(created_at: :desc) }
   has_many :answers, dependent: :destroy
   
   validates :title, presence: true
