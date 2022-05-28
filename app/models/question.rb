c64623de3e97e8c0671c4d34ed2f1010d2315335
uk-m <mymk426@kyf.biglobe.ne.jp>
2022-05-28 13:47:56 +0000

    カテゴリー機能修正

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,9 @@
 class Question < ApplicationRecord
   belongs_to :user
-  default_scope -> { order(created_at: :desc) }
+  belongs_to :category
   has_many :answers, dependent: :destroy
-  has_many :categories, dependent: :destroy
+  default_scope -> { order(created_at: :desc) }
   
   validates :title, presence: true
-  validates :content, presence: true, length: { minimum: 10 }
+  validates :content, presence: true, length: { minimum: 5 }
 end
