70f9de1c3045932ba1ac1287e27265b844a92a84
uk-m <mymk426@kyf.biglobe.ne.jp>
2022-05-30 14:59:00 +0000

    user一覧ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i(edit update destroy)
+  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :admin_user,     only: :destroy
+  
+  def index
+    @users = User.all
+  end
   
   def new
     @user = User.new(flash[:user])
@@ -44,5 +49,9 @@ class UsersController < ApplicationController
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
