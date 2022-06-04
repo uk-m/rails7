2954eb70a79dbf1af54b39057cdcd9109e68378e
uk-m <mymk426@kyf.biglobe.ne.jp>
2022-06-04 13:46:18 +0000

    フォロー機能追加のためモデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
   has_one_attached :image
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   
   validates :name, presence: true, length: { maximum: 25 }
   before_save { self.email = email.downcase }
@@ -46,5 +54,17 @@ class User < ApplicationRecord
   
   def display_image
     image.variant(resize_to_limit: [500, 500])
+  end
+  
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
