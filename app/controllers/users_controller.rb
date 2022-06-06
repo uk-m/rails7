1dd2d7f180bcded407e88b13fb6b4b001567e6df
uk-m <mymk426@kyf.biglobe.ne.jp>
2022-06-06 14:10:51 +0000

    follow機能編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,12 +47,14 @@ class UsersController < ApplicationController
   end
   
   def following
+    @title = "フォロー中"
     @user  = User.find(params[:id])
     @users = @user.following.paginate(page: params[:page])
     render 'show_follow'
   end
 
   def followers
+    @title =　"フォロワー"
     @user  = User.find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
