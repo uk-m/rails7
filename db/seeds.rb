382233a3efa15ba903906ccc6b6c3923d7d38653
uk-m <mymk426@kyf.biglobe.ne.jp>
2022-06-14 14:16:51 +0000

    adminユーザー追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+User.create!(name: "Admin User",
+             email: "admin@mail.co.jp",
+             password: "password",
+             password_confirmation: "password",
+             admin: true)
 Category.create!([
   { name: 'キャリア' },
   { name: 'スキル'}
