b2e62a5489c6052f0542302b6ec44e0306206c61
uk-m <mymk426@kyf.biglobe.ne.jp>
2022-06-19 12:21:08 +0000

    user検索機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   
   root "questions#index"
   resources :users do
+    collection do
+      get 'search'
+    end
     member do
       get :following, :followers
     end
