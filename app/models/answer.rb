32e5d8636e71a4329d2f535e263098862a45c0c3
uk-m <mymk426@kyf.biglobe.ne.jp>
2022-06-22 14:47:32 +0000

    likeモデル作成

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,8 @@
 class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
+  has_many :likes
+  has_many :liked_users, through: :likes, source: :user
   
   validates :content, presence: true, length: { minimum: 5 }
 end
