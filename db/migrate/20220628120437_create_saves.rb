aad25e922e3e2939265f404016eab3cd5de1a349
uk-m <mymk426@kyf.biglobe.ne.jp>
2022-06-28 12:07:36 +0000

    save機能追加

---
+++ db/migrate/20220628120437_create_saves.rb
@@ -0,0 +1,10 @@
+class CreateSaves < ActiveRecord::Migration[7.0]
+  def change
+    create_table :saves do |t|
+      t.references :question, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
