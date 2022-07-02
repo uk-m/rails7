9b2fb815a2b8b1edaa2f642ddd298839ed8843f2
uk-m <mymk426@kyf.biglobe.ne.jp>
2022-07-02 12:14:18 +0000

    質問検索機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  before_action :set_search
+
+  def set_search
+    @search = Question.ransack(params[:q])
+    @search_questions = @search.result.page(params[:page])
+  end
   
   private
   
