2e6b70367dd7d69b401bb048df7b917b37dbeb32
uk-m <n647a@yahoo.co.jp>
2022-08-02 13:09:42 +0000

    テスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "名前、メール、パスワードがあれば有効な状態であること" do
+    user = User.new(
+      name: "Taro",
+      email: "taro@example.com",
+      password: "password")
+    expect(user).to be_valid
+  end
+  
+  it "名前がなければ無効な状態であること" do
+    user = User.new(name: nil)
+    user.valid?
+  end
+  
+  it "重複したメールアドレスなら無効な状態であること" do
+    User.create(
+      name: "Jiro",
+      email: "tester@example.com",
+      password: "password")
+    user = User.new(
+      name: "Saburo",
+      email: "tester@example.com",
+      password: "password")
+    user.valid?
+  end
 end
