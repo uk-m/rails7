de3aaa81248b102c4685728aecaec8530b9f04e3
uk-m <n647a@yahoo.co.jp>
2022-08-12 10:34:23 +0000

    questionパーシャル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
-  mount_uploader :image, image_uploader
+  mount_uploader :image, ImageUploader
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
