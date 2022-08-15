9b8c36b664dc22f8dedd9042b375eda0de84838a
uk-m <n647a@yahoo.co.jp>
2022-08-15 05:02:07 +0000

    レイアウト修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
+  has_one_attached :image
   mount_uploader :image, ImageUploader
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
