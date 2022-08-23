7ec3f019a3d68ff7f1fd8fd9bd60f5e7d024e3e4
uk-m <n647a@yahoo.co.jp>
2022-08-23 13:06:25 +0000

    interest like編集

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -2,22 +2,22 @@ class InterestsController < ApplicationController
   before_action :logged_in_user, only: %i(create destroy)
   
   def create
-    @interest = current_user.interests.create(question_id: params[:question_id])
+    @interest = current_user.interests.new(question_id: params[:question_id])
     @question = @interest.question
     if @interest.save
       respond_to do |format|
-        format.html { redirect_to @question }
+        # format.html { redirect_to @interest }
         format.turbo_stream
       end
     end
   end
 
   def destroy
-    @interest = Interest.find_by(id: params[:id])
+    @interest = Interest.find(params[:id])
     @question = @interest.question
     if @interest.destroy
       respond_to do |format|
-        format.html { redirect_to @question, status: :see_other }
+        # format.html { redirect_to @interest, status: :see_other }
         format.turbo_stream
       end
     end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,21 +2,21 @@ class LikesController < ApplicationController
   before_action :logged_in_user, only: %i(create destroy)
   
   def create
-    @like = current_user.likes.create(answer_id: params[:answer_id])
+    @like = current_user.likes.new(user_id: params[:user_id], answer_id: params[:answer_id])
     if @like.save
       respond_to do |format|
-        format.html { redirect_to @answer }
+        # format.html { redirect_to @like }
         format.turbo_stream
       end
     end
   end
 
   def destroy
-    @like = Like.find_by(id: params[:id])
+    @like = Like.find(params[:id])
     @answer = @like.answer
     if @like.destroy
       respond_to do |format|
-        format.html { redirect_to @answer, status: :see_other }
+        # format.html { redirect_to @like, status: :see_other }
         format.turbo_stream
       end
     end
