198d33a825a48598f64f087eb8b62d507b60035d
uk-m <n647a@yahoo.co.jp>
2022-08-30 12:49:12 +0000

    question systemテスト追加

--- spec/system/questions_spec.rb
+++ spec/system/questions_spec.rb
@@ -2,5 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "Questions", type: :system do
   scenario "ユーザーは新しい質問を作成する" do
+    user = FactoryBot.create(:user)
+    
+    visit root_path
+    click_link "ログイン"
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: user.pasword
+    click_button "ログイン"
+    
+    expect {
+      click_link "質問投稿"
+      fill_in "内容", with: "Test Content"
+      select "業界", from: "Category.all"
+      click_button "投稿する"
+    }.to change(user.questions, :count).by(1)
   end
 end
